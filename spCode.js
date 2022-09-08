b25806f7f82d0a96ad19e7b2955ffe3dc75ea64f
Glitch (silly-sylvia) <none>
2022-09-08 17:16:58 +0000

    🥔🔗 Updated with Glitch

--- spCode.js
+++ spCode.js
@@ -1,16 +1,19 @@
 export const spCode =  `
-  rotateY(mouse.x * PI / 2 + time*.5);
+  let r = getRayDirection();
+  rotateY(mouse.x * PI / 2 + time*.3);
   rotateX(mouse.y * PI / 2);
-  rotateY(getRayDirection().y*4+time);
+  rotateX(r.z*10 + time);
+  rotateY(r.z*10 + time);
 
   let f = fresnel(3);
   color(hsv2rgb(vec3(f*3 + 0.3*time,0.8,1)));
   metal(1.0);
   shine(1.0);
   
-  let scale = 1.5;
+  let scale = 2.5;
   let s = getSpace();
-  let n = 0.3*noise(scale * s + time * 0.2);
+  let t = vec3(-time,time,-time);
+  let n = 0.45*noise(scale * s + t * 1);
   sphere(0.4);
   expand(n);
 `;
